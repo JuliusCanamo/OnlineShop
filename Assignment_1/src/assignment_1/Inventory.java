555fc02d69ec754ea955e30690c0170fe08067b6
JuliusCanamo <jqd2690@autuni.ac.nz>
2025-03-10 14:54:29 +1300

    Use this from now on

---
+++ Assignment_1/src/assignment_1/Inventory.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package assignment_1;
+
+/**
+ *
+ * @author simpl
+ */
+public class Inventory extends Products{
+    
+    private Products[] inventory;
+    
+    public Inventory (){
+        super("Null", "Null", 0.0);
+    }
+}
