65bc1f104b04c8327d6404d1662b4b7eb6ab5cbe
Laraib29994 <ksk0308@aut.ac.nz>
2025-03-16 12:59:12 +1300

    test

---
+++ OnlineShop/src/Assignment_1/OnlineShop.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package assignment_1;
+
+/**
+ *
+ * @author simpl
+ */
+public class OnlineShop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        Category product1=new Category("Pants");
+        
+        product1.setItemType("Pants");
+        product1.printInfo();
+    }
+}
